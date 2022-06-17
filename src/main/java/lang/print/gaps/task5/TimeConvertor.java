ab3531d80dfdc2ccef5727d1da7a5d957acfad94
myka1 <msmykolas@gmail.com>
2022-06-17 21:23:37 +0300

    Update TimeConvertor.java

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,12 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        float seconds = minutes * 60;
+        float seconds = minutes * 60f;
         System.out.println(seconds);
+    }
+
+    public static void main(String[] args) {
+        TimeConvertor ten = new TimeConvertor();
+        ten.convert(10f);
     }
 }
